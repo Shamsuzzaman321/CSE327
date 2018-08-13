76be00bd574db44027fef75801fca5970d3fd993
Shamsuzzaman321 <42300174+Shamsuzzaman321@users.noreply.github.com>
2018-08-14 01:12:13 +0600

    Random Question Generator with php

---
+++ AutoQPGen-master/welcome.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<?php
+?>
+
+<head>
+		
+	 <meta charset="UTF-8">
+    <title>Welcome</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	  <div class="wrapper">
+	<div class="container">
+		<br><br>
+		<img src="logo.png">
+		<h1>Welcome to The Project of Random question generator</h1>
+		
+	<br><br><br><br><br>
+	
+	<form method="post">	
+	<button name="fac" type="button" id="login-faculty" onClick="location.href='login_fac.html'">Login as Faculty</button>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<button name="admin" type="button" id="login_admin" onClick="location.href='login_admin.php'">Login as Administrator</button>	
+	<br><br><br>
+	<footer>
+  <h1>CSE327 Project Created By Group H Coded/Edited By Md.Shamsuzzaman</h1>
+</footer>
+	</form>
+	
+	</div>
+		<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	</div>
+</body>
+
+
+</html>
